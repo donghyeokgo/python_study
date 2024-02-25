e73683410ad560fcb346f593ea7b4861dd83086e
donghyeokgo <donghyeokgo@gmail.com>
2024-02-25 16:42:25 +0900

    List Comprehension

--- beakjoon 3052.py
+++ beakjoon 3052.py
@@ -8,5 +8,8 @@ result = list(set(X % 42 for X in numlist)) # List Comprehension
 
 print(len(result))
 
-
-
+# GPT Solution
+'''
+numbers = [int(input()) % 42 for _ in range(10)]
+print(len(set(numbers)))
+'''
